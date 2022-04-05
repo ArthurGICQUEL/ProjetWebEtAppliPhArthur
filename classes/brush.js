db41988829a73e8780f890723a1f7624c5cfd9d2
PH_Neutral <pierrehoel.charny@gmail.com>
2022-04-05 15:06:02 +0200

    brush init

--- classes/brush.js
+++ classes/brush.js
@@ -1,6 +1,15 @@
+import { Heightmap } from "./heightMap";
+
 export class Brush {
-  
+  /**
+   * 
+   * @param {Heightmap} heightmap 
+   */
   constructor(heightmap) {
     this.heightmap = heightmap;
+  }
+
+  preview() {
+    
   }
 }

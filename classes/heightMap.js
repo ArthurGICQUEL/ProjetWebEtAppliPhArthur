dc6c83209085fa4bc55e12a2f230ecdff5aaff75
Arthur Gicquel <arthurgcl@orange.fr>
2022-04-05 16:48:58 +0200

    lelel

--- classes/heightMap.js
+++ classes/heightMap.js
@@ -1,4 +1,5 @@
 const colors = ["#12522e", "#a88977"];
+const waterColor = "#4287f5";
 
 export class Heightmap {
   constructor(height, width, maxDepth) {
@@ -18,11 +19,14 @@ export class Heightmap {
   display(ctx) {
     for (let x = 0; x < this.pixels.length; x++) {
       for (let y = 0; y < this.pixels[x].length; y++) {
-        ctx.fillStyle = this.lerpColor(
-          colors[0],
-          colors[1],
-          this.pixels[x][y].terrain / this.maxDepth
-        );
+        ctx.fillStyle =
+          this.pixels[x][y].water > 0
+            ? waterColor
+            : this.lerpColor(
+                colors[0],
+                colors[1],
+                this.pixels[x][y].terrain / this.maxDepth
+              );
         ctx.fillRect(
           this.pixelSize * x,
           this.pixelSize * y,
@@ -31,6 +35,51 @@ export class Heightmap {
         );
       }
     }
+  }
+
+  waterBehavior() {
+    for (let x = 0; x < this.pixels.length; x++) {
+      for (let y = 0; y < this.pixels[x].length; y++) {
+        let lowerPoints = this.getLowerPoints();
+        if (lowerPoints.length == 0) return;
+        lowerPoints[Math.random(0, lowerPoints.length)].water++;
+      }
+    }
+  }
+
+  addWater(x, y, radius, depth) {
+    for (let i = -radius; i < radius; i++) {
+      for (let j = -radius; j < radius; j++) {
+        if (i * i + j * j <= radius * radius) {
+          this.pixels[x + i][y + j].water += depth;
+        }
+      }
+    }
+  }
+
+  getLowerPoints(x, y) {
+    let result = [];
+
+    if (
+      pixels[x - 1][y].terrain + pixels[x - 1][y].water <
+      pixels[x][y].terrain + pixels[x][y].water
+    )
+      result.push(pixels[x - 1][y]);
+    if (
+      pixels[x + 1][y].terrain + pixels[x + 1][y].water <
+      pixels[x][y].terrain + pixels[x][y].water
+    )
+      result.push(pixels[x + 1][y]);
+    if (
+      pixels[x][y - 1].terrain + pixels[x][y - 1].water <
+      pixels[x][y].terrain + pixels[x][y].water
+    )
+      result.push(pixels[x][y - 1]);
+    if (
+      pixels[x][y + 1].terrain + pixels[x][y + 1].water <
+      pixels[x][y].terrain + pixels[x][y].water
+    )
+      result.push(pixels[x][y + 1]);
   }
   /**
    * A linear interpolator for hexadecimal colors
